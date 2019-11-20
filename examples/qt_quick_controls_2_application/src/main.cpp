9dc6a0e48d1940ed57c002ce959a6c398f378c01
Alexis Lopez Zubieta <contact@azubieta.net>
2019-11-19 22:02:19 -0600

    Add qt_queck_controls_2_application example

---
+++ examples/qt_quick_controls_2_application/src/main.cpp
@@ -0,0 +1,12 @@
+#include <QGuiApplication>
+#include <QQmlApplicationEngine>
+
+int main(int argc, char *argv[])
+{
+    QGuiApplication app(argc, argv);
+
+    QQmlApplicationEngine engine;
+    engine.load(QUrl(QLatin1String("qrc:/main.qml")));
+
+    return app.exec();
+}
