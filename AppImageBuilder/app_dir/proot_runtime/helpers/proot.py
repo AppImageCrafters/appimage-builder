84baa1dae99760f540e9e44c8618c0186927b829
Alexis Lopez Zubieta <contact@azubieta.net>
2020-02-17 10:54:02 -0600

    don't use linker while proot is statically linked

--- AppImageBuilder/app_dir/proot_runtime/helpers/proot.py
+++ AppImageBuilder/app_dir/proot_runtime/helpers/proot.py
@@ -12,6 +12,7 @@
 import fnmatch
 import logging
 import os
+import subprocess
 
 from AppImageBuilder.app_dir.runtime.helpers.base_helper import BaseHelper
 
@@ -26,8 +27,25 @@ class PRoot(BaseHelper):
         if path:
             app_run.env['PROOT_PATH'] = '${APPDIR}/%s' % path
             app_run.env['PROOT_NO_SECCOMP'] = 1
+
+            if self._is_statically_linked(path):
+                app_run.sections['EXEC'] = [
+                    '# Launch application using a fake root file system',
+                    '${PROOT_PATH} -R ${APPDIR} -b /:/host_root -b /usr/share/icons -b /usr/share/mime -w '
+                    '/host_root/$PWD /${BIN_PATH} ${EXEC_ARGS}',
+                    ''
+                ]
         else:
             raise PRootError('Unable to find proot binary')
 
     def _get_proot_path(self):
         return self._get_glob_relative_file_path('*/bin/proot')
+
+    def _is_statically_linked(self, path):
+        proc_env = os.environ.copy()
+        proc_env['LC_ALL'] = 'LC_ALL=C.UTF-8'
+        process = subprocess.run(['file', path], stdout=subprocess.PIPE, env=proc_env, cwd=self.app_dir)
+
+        output = process.stdout.decode('utf-8')
+
+        return 'statically linked' in output
