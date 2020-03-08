9c7cf252f827a197ef541db82e2dd2ddad303a44
Alexis Lopez Zubieta <contact@azubieta.net>
2020-03-08 13:06:04 -0600

    Bump versio to 0.4.5

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="appimage_builder",
-    version="0.4.5",
+    version="0.4.6",
     author="Alexis Lopez Zubieta",
     author_email="contact@azubieta.net",
     description="Recipe based AppImage creation meta-tool",
