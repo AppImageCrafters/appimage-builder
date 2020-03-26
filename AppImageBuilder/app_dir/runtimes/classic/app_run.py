d25691ef6dcf5103635f95822b03366d8b1f9b87
Alexis Lopez Zubieta <contact@azubieta.net>
2020-03-25 20:10:24 -0600

    fix classic AppRun generator

--- AppImageBuilder/app_dir/runtimes/classic/app_run.py
+++ AppImageBuilder/app_dir/runtimes/classic/app_run.py
@@ -40,41 +40,8 @@ class AppRun:
         ],
         'LINKER': [
             '# Work around for not supported $ORIGIN in the elf PT_INTERP segment',
-            'ln -s ${LINKER_PATH} /tmp/appimage_$APPIMAGE_UUID.ld.so --force',
+            'cp -s ${LINKER_PATH} /tmp/appimage_$APPIMAGE_UUID.ld.so --force',
             ''
-        ],
-        'LIBC': [
-            '###',
-            '# Select the greater libc to run the app',
-            '###',
-            '',
-            '# Configure appdir loader initially',
-            'ln -s ${LINKER_PATH} /tmp/appimage_$APPIMAGE_UUID.ld.so --force',
-            'echo "AppRun -- resolving greater libc --"',
-            '',
-            'SYSTEM_LIBC_PATH=$("${LINKER_PATH}" --list /bin/bash | grep "libc.so" | cut -f 3 -d " ")',
-            'SYSTEM_LIBC_VERSION=$(readlink -f "${SYSTEM_LIBC_PATH}" | rev | cut -d / -f 1 | cut -d "-" -f 1 | cut -d '
-            '"." -f 2- | rev)',
-            'echo "AppRun -- system libc: $SYSTEM_LIBC_PATH $SYSTEM_LIBC_VERSION"',
-            '',
-            'APPDIR_LIBC_PATH=$("${LINKER_PATH}" --list $APPDIR/$BIN_PATH | grep "libc.so" | cut -f 3 -d " ")',
-            'APPDIR_LIBC_VERSION=$(readlink -f "${APPDIR_LIBC_PATH}" | rev | cut -d / -f 1 | cut -d "-" -f 1 | cut -d '
-            '"." -f 2- | rev)',
-            'echo "AppRun -- appdir libc: $APPDIR_LIBC_PATH $APPDIR_LIBC_VERSION"',
-            '',
-            'GREATER_LIBC=$(printf "$SYSTEM_LIBC_VERSION\n$APPDIR_LIBC_VERSION"  | sort -V | tail -1)',
-            '',
-            'if [ "$SYSTEM_LIBC_VERSION" == "$GREATER_LIBC" ]; then',
-            '  echo "AppRun -- Using System libc version: $SYSTEM_LIBC_VERSION"',
-            '  LIBC_DIR=$(dirname $SYSTEM_LIBC_PATH)',
-            '  export LINKER_PATH=$LIBC_DIR/ld-*.so',
-            '  export LD_LIBRARY_PATH="$LIBC_DIR;$LD_LIBRARY_PATH"',
-            '',
-            '  # use system loader',
-            '  ln -s ${LINKER_PATH} /tmp/appimage_$APPIMAGE_UUID.ld.so --force',
-            'else',
-            '  echo "AppRun -- Using AppDir libc version: $APPDIR_LIBC_VERSION"',
-            'fi'
         ],
         'EXEC': [
             '# Launch application',
@@ -105,11 +72,13 @@ class AppRun:
         file_lines.extend(self.sections['HEADER'])
         file_lines.extend(self.sections['APPDIR'])
 
-        file_lines.extend(self._generate_env_section())
-
         if self.env['LINKER_PATH']:
+            file_lines.extend(self._generate_env('APPIMAGE_UUID', self.env['APPIMAGE_UUID']))
+            file_lines.extend(self._generate_env('LINKER_PATH', self.env['LINKER_PATH']))
+
             file_lines.extend(self.sections['LINKER'])
-            file_lines.extend(self.sections['LIBC'])
+
+        file_lines.extend(self._generate_env_section())
 
         for k, v in self.sections.items():
             if k not in ['HEADER', 'APPDIR', 'LINKER', 'LIBC', 'EXEC', 'EXEC_ARGS']:
@@ -125,9 +94,13 @@ class AppRun:
         lines = ['', '# Run Environment Setup']
         for k, v in self.env.items():
             if v:
-                line = 'export %s="%s"' % (k, v)
-                lines.append(line)
+                lines.extend(self._generate_env(k, v))
         lines.append('')
+        return lines
+
+    def _generate_env(self, k, v):
+        lines = ['export %s="%s"' % (k, v)]
+
         return lines
 
     def _set_permissions(self, path):
