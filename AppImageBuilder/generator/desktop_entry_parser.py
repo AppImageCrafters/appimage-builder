4d5b42d3285be0106563244137efb15cf7ef5a0d
Alexis Lopez Zubieta <contact@azubieta.net>
2020-04-24 16:51:03 -0500

    parse app_info from desktop file

---
+++ AppImageBuilder/generator/desktop_entry_parser.py
@@ -0,0 +1,63 @@
+#  Copyright  2020 Alexis Lopez Zubieta
+#
+#  Permission is hereby granted, free of charge, to any person obtaining a
+#  copy of this software and associated documentation files (the "Software"),
+#  to deal in the Software without restriction, including without limitation the
+#  rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+#  sell copies of the Software, and to permit persons to whom the Software is
+#  furnished to do so, subject to the following conditions:
+#
+#  The above copyright notice and this permission notice shall be included in
+#  all copies or substantial portions of the Software.
+
+import configparser
+
+
+class DesktopFileParser:
+    def __init__(self, file_path):
+        self.parser = configparser.ConfigParser()
+        self.parser.read(file_path)
+
+        exec = self.parser['Desktop Entry']['Exec'].strip()
+        self.exec_path, self.exec_args = self._split_exec_path_and_args(exec)
+
+        # convert desktop file exec args to bash notation
+        self.exec_args = self.exec_args.replace('%f', '$@')
+        self.exec_args = self.exec_args.replace('%F', '$@')
+        self.exec_args = self.exec_args.replace('%U', '$@')
+        self.exec_args = self.exec_args.replace('%u', '$@')
+
+    @staticmethod
+    def _split_exec_path_and_args(exec):
+        if exec[0] == '\'' or exec[0] == '\"':
+            end = exec.find(exec[0], 1)
+            while end != -1 and exec[end - 1] == '\\':
+                end = exec.find(exec[0], end + 1)
+            if end == -1:
+                end = len(exec)
+
+            exec_path = exec[1:end].strip()
+            exec_args = exec[end + 1:].strip()
+
+            return exec_path, exec_args
+        else:
+            end = exec.find(" ")
+            if end == -1:
+                end = len(exec)
+
+            exec_path = exec[:end].strip()
+            exec_args = exec[end + 1:].strip()
+
+            return exec_path, exec_args
+
+    def get_name(self):
+        return self.parser['Desktop Entry']['Name']
+
+    def get_icon(self):
+        return self.parser['Desktop Entry']['Icon']
+
+    def get_exec_path(self):
+        return self.exec_path
+
+    def get_exec_args(self):
+        return self.exec_args
--- AppImageBuilder/generator/generator.py
+++ AppImageBuilder/generator/generator.py
@@ -15,19 +15,28 @@ import logging
 import os
 import questionary
 import time
+import configparser
 from emrichen import Context, Template
 from progress.spinner import Spinner
+
+from AppImageBuilder.generator.desktop_entry_parser import DesktopFileParser
+
+
+class RecipeGeneratorError(RuntimeError):
+    pass
 
 
 class RecipeGenerator:
     def __init__(self):
-        self.app_dir = ''
+        self.app_dir = self._locate_app_dir()
         self.app_info_id = ''
         self.app_info_name = ''
         self.app_info_icon = ''
         self.app_info_version = ''
         self.app_info_exec = ''
         self.app_info_exec_args = ''
+
+        self._setup_app_info()
 
         self.runtime_generator = ''
         self.runtime_env = []
@@ -173,3 +182,53 @@ class RecipeGenerator:
 
         logging.debug(appimage_builder_yml_template.render(appimage_builder_yml_ctx))
 
+    @staticmethod
+    def _locate_app_dir():
+        for file_name in os.listdir(os.path.curdir):
+            if os.path.isdir(file_name) and file_name.lower() == 'appdir':
+                return file_name
+
+        raise RecipeGeneratorError('Unable to find an AppDir, this is required to create a recipe.')
+
+    def _setup_app_info(self):
+        desktop_files = self._find_desktop_entry_files()
+        desktop_file = None
+        if len(desktop_files) == 1:
+            desktop_file = desktop_files[0]
+
+        if len(desktop_files) > 1:
+            desktop_file = questionary.select('Main desktop entry :', desktop_files).ask()
+
+        if desktop_file:
+            parser = DesktopFileParser(desktop_file)
+            self.app_info_name = parser.get_name()
+            self.app_info_icon = parser.get_icon()
+            exec = parser.get_exec_path()
+            self.app_info_exec = self._resolve_exec_path(exec)
+            self.app_info_exec_args = parser.get_exec_args()
+
+    def _find_desktop_entry_files(self):
+        desktop_entries = []
+        for file_name in os.listdir(os.path.abspath(self.app_dir)):
+            if file_name.lower().endswith('desktop'):
+                desktop_entries.append(file_name)
+
+        for root, dir, files in os.walk(os.path.join(self.app_dir, 'usr', 'share', 'applications')):
+            for file_name in files:
+                if file_name.lower().endswith('desktop'):
+                    desktop_entries.append(os.path.join(root, file_name))
+
+        return desktop_entries
+
+    def _resolve_exec_path(self, exec):
+        if '/' in exec and os.path.exists(os.path.join(self.app_dir, exec)):
+            return exec
+
+        absolute_app_dir = os.path.abspath(self.app_dir)
+        for root, dir, files in os.walk(absolute_app_dir):
+            for file in files:
+                full_path = os.path.join(root, file)
+                if os.access(full_path, os.X_OK):
+                    return os.path.relpath(full_path, absolute_app_dir)
+
+        raise RecipeGeneratorError('Unable to find executable: %s' % exec)
