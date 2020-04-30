21582b68e95ff15120772540673b44ca0e741701
Alexis Lopez Zubieta <contact@azubieta.net>
2020-04-30 13:38:30 -0500

    remove nested dependencies from the include list

--- AppImageBuilder/generator/apt_recipe_generator.py
+++ AppImageBuilder/generator/apt_recipe_generator.py
@@ -36,14 +36,36 @@ class AptRecipeGenerator:
 
     @staticmethod
     def resolve_includes(runtime_libs):
+        packages = AptRecipeGenerator._map_files_to_packages(runtime_libs)
+        packages = AptRecipeGenerator._remove_excluded_packages(packages)
+        packages = AptRecipeGenerator._remove_nested_dependencies(packages)
+
+        return list(sorted(packages))
+
+    @staticmethod
+    def _map_files_to_packages(runtime_libs):
         dpkg_query = DpkgQuery()
         packages = dpkg_query.search(runtime_libs)
+        return packages
+
+    @staticmethod
+    def _remove_nested_dependencies(packages):
+        dpkg_query = DpkgQuery()
+        dependencies = dpkg_query.depends(packages)
+        for pkd_name, pkg_depends in dependencies.items():
+            for dependency in pkg_depends:
+                if dependency in packages:
+                    packages.remove(dependency)
+
+        return packages
+
+    @staticmethod
+    def _remove_excluded_packages(packages):
         exclusion_list = []
         exclusion_list.extend(apt_core_packages)
         exclusion_list.extend(apt_xclient_packages)
         exclusion_list.extend(apt_graphics_stack_packages)
         exclusion_list.extend(apt_font_config_packages)
-
         filtered_packages = set()
         for package in packages:
             excluded = False
@@ -53,8 +75,7 @@ class AptRecipeGenerator:
 
             if not excluded:
                 filtered_packages.add(package)
-
-        return list(sorted(filtered_packages))
+        return filtered_packages
 
     @staticmethod
     def _read_sources_list_file(path):
