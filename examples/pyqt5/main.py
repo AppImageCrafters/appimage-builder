8314dfdaaec0e33a0457572a6f5017a62b1d6306
Alexis Lopez Zubieta <contact@azubieta.net>
2020-07-02 13:35:20 -0500

    Add pyqt5 example

---
+++ examples/pyqt5/main.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+import sys
+from PyQt5.QtWidgets import QApplication, QLabel, QMainWindow
+from PyQt5.QtCore import Qt
+from PyQt5 import QtGui
+from pyfiglet import Figlet
+
+
+# Subclass QMainWindow to customise your application's main window
+class MainWindow(QMainWindow):
+
+    def __init__(self, *args, **kwargs):
+        super(MainWindow, self).__init__(*args, **kwargs)
+
+        self.setWindowTitle("My Awesome App")
+        f = Figlet(font='digital')
+        label = QLabel(f.renderText("Hello World!"))
+        label.setFont(QtGui.QFont("monospace", 14, QtGui.QFont.Black))
+
+        label.setAlignment(Qt.AlignCenter)
+
+        self.setCentralWidget(label)
+
+
+app = QApplication(sys.argv)
+
+window = MainWindow()
+window.show()
+
+app.exec_()
