1e0f9e46e938390e94a1e40a8eb2fd06565412d4
Alexis Lopez Zubieta <contact@azubieta.net>
2020-09-06 22:21:03 -0500

    Improve tests logging

--- AppImageBuilder/app_dir/tester.py
+++ AppImageBuilder/app_dir/tester.py
@@ -12,8 +12,6 @@
 import logging
 import os
 import re
-import stat
-import tempfile
 
 import docker
 
@@ -31,9 +29,7 @@ class Tester:
             self.use_host_x = False
             self.env = []
 
-            self.temp_dir = None
-            self.tests_wrapper_path = None
-            self.client = None
+            self.client = docker.from_env()
             self.logger = logging.getLogger("TEST CASE '%s'" % self.name)
 
         def run(self):
@@ -41,23 +37,32 @@ class Tester:
             logging.info("Running test: %s" % self.name)
             logging.info("-----------------------------")
 
-            self.logger.info("Executing: %s" % self.command)
-            ctr = self._run_container()
-            self._print_container_logs(ctr)
-            result = ctr.wait()
-
-            if result['StatusCode'] != 0:
-                raise Tester.TestFailed(result['Error'])
-
-        def _run_container(self):
             volumes = self._get_container_volumes()
-            command = self._get_container_command()
             environment = self.get_container_environment()
 
-            ctr = self.client.containers.run(self.image, command, auto_remove=True, working_dir='/app',
-                                             volumes=volumes, stdout=True, stderr=True, cap_add=['SYS_PTRACE'],
-                                             environment=environment, detach=True, devices=['/dev/snd'])
-            return ctr
+            container = self.client.containers.run(self.image, '/bin/sh', working_dir='/app',
+                                                   volumes=volumes, environment=environment, devices=['/dev/snd'],
+                                                   cap_add=['SYS_PTRACE'], tty=True, detach=True, auto_remove=True)
+
+            try:
+                self.logger.info("before command")
+                self._run_command('useradd -mu %s %s' % (os.getuid(), os.getenv("USER")), container)
+                self._run_command('mkdir -p /home/%s/.config' % os.getenv("USER"), container, user=os.getenv("USER"))
+
+                self.logger.info("command")
+                self._run_command(self.command, container, user=os.getenv("USER"))
+            finally:
+                container.kill()
+
+        def _run_command(self, command, container, user='root'):
+            print("$ %s" % command)
+            exit_code, output = container.exec_run(command, user=user, tty=True)
+            for line in output.decode('utf-8').splitlines():
+                print(line)
+
+            if exit_code != 0:
+                print("$ %s FAILED, exit code: %s" % (command, exit_code))
+                raise Tester.TestFailed()
 
         def _print_container_logs(self, ctr):
             logs = ctr.logs(stream=True)
@@ -69,7 +74,6 @@ class Tester:
 
             if self.use_host_x:
                 volumes['/tmp/.X11-unix'] = {'bind': '/tmp/.X11-unix', 'mode': 'rw'}
-                volumes[self.temp_dir.name] = {'bind': self.temp_dir.name, 'mode': 'rw'}
 
             dbus_session_address = os.getenv('DBUS_SESSION_BUS_ADDRESS')
 
@@ -80,35 +84,6 @@ class Tester:
                     volumes[search_result.group(1)] = {'bind': search_result.group(1), 'mode': 'rw'}
 
             return volumes
-
-        def setup(self):
-            self.temp_dir = tempfile.TemporaryDirectory()
-            self.tests_wrapper_path = os.path.join(self.temp_dir.name, 'test_wrapper.sh')
-            self.client = docker.from_env()
-            if self.use_host_x:
-                self._write_host_x_share_wrapper()
-
-        def teardown(self):
-            self.temp_dir.cleanup()
-
-        def _write_host_x_share_wrapper(self):
-            with open(self.tests_wrapper_path, 'w') as f:
-                f.write('\n'.join([
-                    '#!/bin/sh',
-                    'set -e',
-                    'useradd -mu $UID $UNAME',
-                    'export HOME=/home/$UNAME'
-                    'export XDG_DATA_DIRS=/usr/share',
-                    'su $UNAME -c "$@"',
-                ]))
-
-            os.chmod(self.tests_wrapper_path, stat.S_IRWXU | stat.S_IXGRP | stat.S_IRGRP | stat.S_IXOTH | stat.S_IROTH)
-
-        def _get_container_command(self):
-            if self.use_host_x:
-                return [self.tests_wrapper_path, self.command]
-            else:
-                return self.command
 
         def get_container_environment(self):
             if self.use_host_x:
@@ -154,11 +129,8 @@ class Tester:
 
         for test in self.tests:
             try:
-                test.setup()
                 test.run()
             except Tester.TestFailed:
                 raise
             except Exception as e:
                 raise Tester.TestFailed("Execution failed. Error message: %s" % e)
-            finally:
-                test.teardown()
