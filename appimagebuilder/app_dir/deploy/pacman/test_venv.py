4edd7e2784f78edd4294600ad6df4aa674dd234e
Alexis Lopez Zubieta <contact@azubieta.net>
2020-12-02 13:23:31 -0600

    Generate pacman.conf
    Allows:
      - custom repositories
      - setting arch

--- appimagebuilder/app_dir/deploy/pacman/test_venv.py
+++ appimagebuilder/app_dir/deploy/pacman/test_venv.py
@@ -25,22 +25,35 @@ class TestVenv(TestCase):
     def setUpClass(cls):
         cls.appdir_path = Path("/tmp/AppDir")
         cls.venv_path = Path("/tmp/pacman-venv")
-        cls.pacman_venv = Venv(cls.venv_path, repositories={}, keys=[], architectures=[])
+        cls.pacman_venv = Venv(
+            cls.venv_path,
+            repositories={
+                "core": ["https://mirror.rackspace.com/archlinux/$repo/os/$arch"]
+            },
+            architecture="auto",
+        )
         cls.pacman_venv.update()
         cls.appdir_path.mkdir(parents=True, exist_ok=True)
 
     @classmethod
     def tearDownClass(cls):
-        shutil.rmtree(cls.venv_path)
-        shutil.rmtree(cls.appdir_path)
+        pass
+        # shutil.rmtree(cls.venv_path)
+        # shutil.rmtree(cls.appdir_path)
 
     def test_retrieve(self):
-        self.pacman_venv.retrieve(["bash"], ["tzdata", "filesystem", "linux-api-headers"])
+        self.pacman_venv.retrieve(
+            ["bash"], ["tzdata", "filesystem", "linux-api-headers"]
+        )
 
     def test_extract(self):
-        files = self.pacman_venv.retrieve(["bash"], ["tzdata", "filesystem", "linux-api-headers"])
+        files = self.pacman_venv.retrieve(
+            ["bash"], ["tzdata", "filesystem", "linux-api-headers"]
+        )
         self.pacman_venv.extract(files[0], self.appdir_path)
 
     def test_read_package_data(self):
-        files = self.pacman_venv.retrieve(["bash"], ["tzdata", "filesystem", "linux-api-headers"])
+        files = self.pacman_venv.retrieve(
+            ["bash"], ["tzdata", "filesystem", "linux-api-headers"]
+        )
         self.assertTrue(self.pacman_venv.read_package_data(files[0]))
