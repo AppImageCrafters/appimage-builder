AppImageCrafters/appimage-builder

commit a937ac998e0235a376614ab5664194fd77c7d84d

fix: remove redundant logging

appimagebuilder/app_dir/deploy/pacman/venv.py
@@ -130,7 +130,6 @@ class Venv:
             "--print-format '%%l' " \
             "--noconfirm {exclude} {packages}"
         self._logger.debug(command)
-        print(exclude_str, packages_str)
         output = self._run_command(command,
                                    packages=packages_str,
                                    exclude=exclude_str,

commit 793165944903c05d4172d65c5de23e58bded8a3c

fix: wait infinitely for a process to complete
Regression introduced by #67

appimagebuilder/app_dir/deploy/pacman/venv.py
@@ -195,7 +195,7 @@ class Venv:
         stdout=sys.stdout,
         assert_success=True,
         wait_for_completion=True,
-        wait_for_completion_timeout=6000,
+        wait_for_completion_timeout=None,
         **kwargs
     ):
         """