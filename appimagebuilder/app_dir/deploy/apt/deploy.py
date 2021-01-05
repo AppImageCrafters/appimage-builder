4a484a06f3fe789a32ff7e1e7a4c12e9bb288521
Alexis Lopez Zubieta <contact@azubieta.net>
2021-01-04 18:16:33 -0600

    Ref: move package listings to a separated file

--- appimagebuilder/app_dir/deploy/apt/deploy.py
+++ appimagebuilder/app_dir/deploy/apt/deploy.py
@@ -14,87 +14,19 @@ import logging
 import os
 from pathlib import Path
 
+from . import listings
 from .venv import Venv
 
 
 class Deploy:
     """Deploy deb packages into an AppDir using apt-get to resolve the packages and their dependencies"""
 
-    # manually crafted lists of packages used to determine what should be excluded by default or deployed in
-    # a different partition
-    listings = {
-        "glibc": ["libc6", "zlib1g", "libstdc++6"],
-        # packages that apt and dpkg assume as installed
-        "apt_core": ["dpkg", "debconf", "dpkg", "apt"],
-        # system service packages are usually safe to exclude
-        "system_services": [
-            "util-linux",
-            "coreutils",
-            "adduser",
-            "avahi-daemon",
-            "base-files",
-            "bind9-host",
-            "consolekit",
-            "dbus",
-            "debconf",
-            "dpkg",
-            "lsb-base",
-            "libcap2-bin",
-            "libinput-bin",
-            "multiarch-support",
-            "passwd",
-            "systemd",
-            "systemd-sysv",
-            "ucf",
-            "iso-codes",
-            "shared-mime-info",
-            "mount",
-            "xdg-user-dirs",
-            "sysvinit-utils",
-            "debianutils",
-            "init-system-helpers",
-            "libpam-runtime",
-            "libpam-modules-bin",
-            # fontconfig can be excluded most of the time
-            "libfontconfig*",
-            "fontconfig",
-            "fontconfig-config",
-            "libfreetype*",
-        ],
-        # because of issues with the nvidia driver and to achieve better performance the graphics
-        # stack packages are also excluded by default
-        "graphics": [
-            "libglvnd*",
-            "libglx*",
-            "libgl1*",
-            "libdrm*",
-            "libegl1*",
-            "libegl1-*",
-            "libglapi*",
-            "libgles2*",
-            "libgbm*",
-            "mesa-*",
-            # the following X11 libraries are tightly related to the packages above
-            "x11-common",
-            "libx11-*",
-            "libxcb1",
-            "libxcb-shape0",
-            "libxcb-shm0",
-            "libxcb-glx0",
-            "libxcb-xfixes0",
-            "libxcb-present0",
-            "libxcb-render0",
-            "libxcb-dri2-0",
-            "libxcb-dri3-0",
-        ],
-    }
-
     def __init__(self, apt_venv: Venv):
         self.apt_venv = apt_venv
         self.logger = logging.getLogger("AptPackageDeploy")
 
     def deploy(
-        self, package_names: [str], appdir_root: str, exclude_patterns=None
+            self, package_names: [str], appdir_root: str, exclude_patterns=None
     ) -> [str]:
         """Deploy the packages and their dependencies to appdir_root.
 
@@ -113,7 +45,7 @@ class Deploy:
             )
 
         # set apt core packages as installed, required for it to properly resolve dependencies
-        apt_core_packages = self.apt_venv.search_packages(self.listings["apt_core"])
+        apt_core_packages = self.apt_venv.search_packages(listings.apt_core)
         self.apt_venv.set_installed_packages(apt_core_packages)
 
         # lists packages to be installed including dependencies
@@ -158,7 +90,7 @@ class Deploy:
         appdir_root.mkdir(exist_ok=True, parents=True)
         libc_root.mkdir(exist_ok=True, parents=True)
         # list libc related packages
-        libc_packages = self.apt_venv.install_simulate(self.listings["glibc"])
+        libc_packages = self.apt_venv.install_simulate(listings.glibc)
 
         for package in packages:
             final_target = appdir_root
@@ -176,9 +108,9 @@ class Deploy:
         # remove duplicated
         patterns = set(patterns)
         # don't bundle graphic stack packages
-        patterns = patterns.union(self.listings["graphics"])
+        patterns = patterns.union(listings.graphics)
         # don't bundle system services
-        patterns = patterns.union(self.listings["system_services"])
+        patterns = patterns.union(listings.system_services)
 
         # resolve packages
         packages = self.apt_venv.search_packages(patterns)
