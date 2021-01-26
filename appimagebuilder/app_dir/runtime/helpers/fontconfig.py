68d058ceff3c09e3cca137d0556d661a726c3243
Alexis Lopez Zubieta <contact@azubieta.net>
2021-01-26 14:40:17 -0600

    feature: build font config cache

--- appimagebuilder/app_dir/runtime/helpers/fontconfig.py
+++ appimagebuilder/app_dir/runtime/helpers/fontconfig.py
@@ -11,45 +11,34 @@
 #  all copies or substantial portions of the Software.
 import logging
 import os
+import subprocess
 
 from .base_helper import BaseHelper
 from ..environment import GlobalEnvironment
 
 
 class FontConfig(BaseHelper):
-    fonts_conf_template = """<?xml version="1.0"?>
-<!DOCTYPE fontconfig SYSTEM "fonts.dtd">
-<fontconfig>
-    <dir prefix="relative">{appdir_fonts_dir}</dir>
-    <include ignore_missing="yes" prefix="xdg">fontconfig/fonts.conf</include>
-    <include ignore_missing="yes">conf.d</include>
-    <include ignore_missing="yes">local.conf</include>
-</fontconfig>
-"""
-
     def __init__(self, app_dir, app_dir_cache):
         super().__init__(app_dir, app_dir_cache)
         self.priority = 0
 
     def configure(self, global_env: GlobalEnvironment):
-
         fonts_dir = self.app_dir_cache.find_one("*/share/fonts", ["is_dir"])
         if fonts_dir:
             font_conf_path = os.path.join(self.app_dir, "etc/fonts/fonts.conf")
 
             global_env.set("FONTCONFIG_FILE", font_conf_path)
-            # global_env.set("FONTCONFIG_PATH", fontconfig_path)
-            # global_env.set("FONTCONFIG_SYSROOT", self.app_dir)
+            global_env.set("FONTCONFIG_SYSROOT", self.app_dir)
 
-            self._generate_fonts_conf(fonts_dir, font_conf_path)
+            self._generate_fonts_conf(font_conf_path)
 
-    def _generate_fonts_conf(self, fonts_dir, font_conf_path):
-        font_conf_dir = os.path.dirname(font_conf_path)
-        rel_fonts_dir = os.path.relpath(fonts_dir, font_conf_dir)
-        fonts_conf_data = self.fonts_conf_template.format(
-            appdir_fonts_dir=rel_fonts_dir
+    def _generate_fonts_conf(self, font_conf_path):
+        env = os.environ.copy()
+        env["FONTCONFIG_FILE"] = font_conf_path.__str__()
+        _proc = subprocess.run(
+            ["fc-cache", "-s", "-v", "-f", "--sysroot=%s" % self.app_dir], env=env
         )
-
-        os.makedirs(os.path.dirname(font_conf_path), exist_ok=True)
-        with open(font_conf_path, "w") as file:
-            file.write(fonts_conf_data)
+        if _proc.returncode:
+            raise RuntimeError(
+                '"%s" execution failed with code %s' % (_proc.args, _proc.returncode)
+            )
