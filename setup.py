350b180bcf6a8cf6032ade36b83af1a65d0aea90
Alexis Lopez Zubieta <contact@azubieta.net>
2021-01-29 16:04:02 -0600

    set python package version to 0.8.3

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="appimage_builder",
-    version="0.8.2",
+    version="0.8.3",
     author="Alexis Lopez Zubieta",
     author_email="contact@azubieta.net",
     description="Recipe based AppImage creation meta-tool",
