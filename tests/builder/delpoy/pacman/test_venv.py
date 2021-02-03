11937c41ea1e6aa0ae14598b4bd61b12a627557b
Alexis Lopez Zubieta <contact@azubieta.net>
2021-02-03 17:09:06 -0600

    refactor: move pacman tests into tests dir

--- tests/builder/delpoy/pacman/test_venv.py
+++ tests/builder/delpoy/pacman/test_venv.py
@@ -1,20 +1,20 @@
-#   Copyright  2020 Alexis Lopez Zubieta
+#  Copyright  2021 Alexis Lopez Zubieta
 #
-#   Permission is hereby granted, free of charge, to any person obtaining a
-#   copy of this software and associated documentation files (the "Software"),
-#   to deal in the Software without restriction, including without limitation the
-#   rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
-#   sell copies of the Software, and to permit persons to whom the Software is
-#   furnished to do so, subject to the following conditions:
+#  Permission is hereby granted, free of charge, to any person obtaining a
+#  copy of this software and associated documentation files (the "Software"),
+#  to deal in the Software without restriction, including without limitation the
+#  rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+#  sell copies of the Software, and to permit persons to whom the Software is
+#  furnished to do so, subject to the following conditions:
 #
-#   The above copyright notice and this permission notice shall be included in
-#   all copies or substantial portions of the Software.
+#  The above copyright notice and this permission notice shall be included in
+#  all copies or substantial portions of the Software.
 
 import shutil
 from pathlib import Path
 from unittest import TestCase
 
-from .venv import Venv
+from appimagebuilder.builder.deploy.pacman.venv import Venv
 
 
 class TestVenv(TestCase):
