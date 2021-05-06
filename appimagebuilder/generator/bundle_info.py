999fd4b75becd5d3c7d8470b0948d11bcc8981a3
Alexis Lopez Zubieta <contact@azubieta.net>
2021-05-06 17:30:43 -0500

    Remake bundle information gatherer

---
+++ appimagebuilder/generator/bundle_info.py
@@ -0,0 +1,37 @@
+#  Copyright  2021 Alexis Lopez Zubieta
+#
+#  Permission is hereby granted, free of charge, to any person obtaining a
+#  copy of this software and associated documentation files (the "Software"),
+#  to deal in the Software without restriction, including without limitation the
+#  rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+#  sell copies of the Software, and to permit persons to whom the Software is
+#  furnished to do so, subject to the following conditions:
+#
+#  The above copyright notice and this permission notice shall be included in
+#  all copies or substantial portions of the Software.
+from appimagebuilder.generator.app_info import AppInfo
+
+
+class BundleInfo:
+    """Application information"""
+
+    app_info: AppInfo
+
+    # update string to be attached into
+    update_string: str
+
+    # appimage runtime arch
+    runtime_arch: str
+
+    def __init__(
+        self,
+        app_info: AppInfo = None,
+        update_string: str = None,
+        runtime_arch: str = None,
+    ):
+        if not app_info:
+            self.app_info = AppInfo()
+        else:
+            self.app_info = app_info
+        self.update_string = update_string
+        self.runtime_arch = runtime_arch
---
+++ tests/generator/dummy_path.py
@@ -0,0 +1,35 @@
+#  Copyright  2021 Alexis Lopez Zubieta
+#
+#  Permission is hereby granted, free of charge, to any person obtaining a
+#  copy of this software and associated documentation files (the "Software"),
+#  to deal in the Software without restriction, including without limitation the
+#  rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+#  sell copies of the Software, and to permit persons to whom the Software is
+#  furnished to do so, subject to the following conditions:
+#
+#  The above copyright notice and this permission notice shall be included in
+#  all copies or substantial portions of the Software.
+import fnmatch
+import pathlib
+
+
+class DummyPath:
+    """Mocks pathlib.Path"""
+
+    def __init__(self, path: str, children: [str] = None):
+        self.path = path
+        self.children = children
+
+    def glob(self, pattern):
+        results = []
+        for file in self.children:
+            if fnmatch.fnmatch(file, pattern):
+                results.append(DummyPath(file, []))
+
+        return results
+
+    def __str__(self):
+        return self.path
+
+    def __eq__(self, o: object) -> bool:
+        return self.__class__ == o.__class__ and self.path == o.path
