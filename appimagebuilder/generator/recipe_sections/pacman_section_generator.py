634cad12f8adcfa1a9d4c54119980bdeb834f6c0
Alexis Lopez Zubieta <contact@azubieta.net>
2021-05-18 18:35:37 -0500

    Add pacman support on the generator

---
+++ appimagebuilder/generator/recipe_sections/pacman_section_generator.py
@@ -0,0 +1,50 @@
+#  Copyright  2021 Alexis Lopez Zubieta
+#
+#  Permission is hereby granted, free of charge, to any person obtaining a
+#  copy of this software and associated documentation files (the "Software"),
+#  to deal in the Software without restriction, including without limitation the
+#  rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+#  sell copies of the Software, and to permit persons to whom the Software is
+#  furnished to do so, subject to the following conditions:
+#
+#  The above copyright notice and this permission notice shall be included in
+#  all copies or substantial portions of the Software.
+from appimagebuilder.generator.bundle_info import BundleInfo
+from appimagebuilder.generator.package_managers.pacman.file_package_resolver import (
+    FilePackageResolver,
+)
+from appimagebuilder.generator.recipe_sections.package_manager_recipe_section_generator import (
+    PackageManagerSectionGenerator,
+)
+
+
+class PacmanSectionGenerator(PackageManagerSectionGenerator):
+    def __init__(self, file_package_resolver: FilePackageResolver):
+        self._file_package_resolver = file_package_resolver
+
+    def id(self) -> str:
+        return "pacman"
+
+    def generate(self, dependencies: [str], bundle_info: BundleInfo) -> ({}, [str]):
+        file_package_map = self._file_package_resolver.resolve(dependencies)
+
+        include_list = set(file_package_map.values())
+
+        missing_files = self._find_missing_files(dependencies, file_package_map)
+
+        section = {
+            # "Architecture": "",
+            # "repositories": {},
+            "include": sorted(include_list),
+            "exclude": [],
+            # "options": {"SigLevel": "Optional TrustAll"},
+        }
+
+        return section, missing_files
+
+    def _find_missing_files(self, dependencies, file_package_map):
+        missing_files = set(dependencies)
+        for k in file_package_map.keys():
+            if k in missing_files:
+                missing_files.remove(k)
+        return missing_files
---
+++ tests/package_managers/pacman/test_file_package_resolver.py
@@ -0,0 +1,33 @@
+#  Copyright  2021 Alexis Lopez Zubieta
+#
+#  Permission is hereby granted, free of charge, to any person obtaining a
+#  copy of this software and associated documentation files (the "Software"),
+#  to deal in the Software without restriction, including without limitation the
+#  rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+#  sell copies of the Software, and to permit persons to whom the Software is
+#  furnished to do so, subject to the following conditions:
+#
+#  The above copyright notice and this permission notice shall be included in
+#  all copies or substantial portions of the Software.
+import pathlib
+from unittest import TestCase
+
+from appimagebuilder.generator.package_managers.pacman.file_package_resolver import (
+    FilePackageResolver,
+)
+
+
+class TestFilePackageResolver(TestCase):
+    def test__parse_pacman_f_output(self):
+        sample_pacman_output = """
+usr/bin/xmllint is owned by extra/libxml2 2.9.10-9
+usr/bin/xmlwf is owned by core/expat 2.3.0-1
+usr/bin/xtables-legacy-multi is owned by core/iptables 1:1.8.7-1
+"""
+        result = FilePackageResolver._parse_pacman_f_output(sample_pacman_output)
+        expected = {
+            pathlib.Path("usr/bin/xmllint"): "libxml2",
+            pathlib.Path("usr/bin/xmlwf"): "expat",
+            pathlib.Path("usr/bin/xtables-legacy-multi"): "iptables",
+        }
+        self.assertEqual(result, expected)
