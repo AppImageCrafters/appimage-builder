169a8e6991b7b83a26b95f62b623364a3fbb741e
Alexis Lopez Zubieta <contact@azubieta.net>
2021-05-28 00:28:18 -0500

    Drop builder class

--- appimagebuilder/main/commands/create_appdir_command.py
+++ appimagebuilder/main/commands/create_appdir_command.py
@@ -9,7 +9,6 @@
 #
 #  The above copyright notice and this permission notice shall be included in
 #  all copies or substantial portions of the Software.
-from appimagebuilder.builder import Builder
 from appimagebuilder.main.commands.command import Command
 
 
