afbbc198981f16929d2c7cb36b5704d4d618f01e
Alexis Lopez Zubieta <contact@azubieta.net>
2021-06-21 13:19:38 -0500

    Make elf arch reading tests dependent on the existence of the ld-linux binaries

--- tests/utils/test_elf.py
+++ tests/utils/test_elf.py
@@ -1,14 +1,14 @@
-from unittest import TestCase
+import os.path
+from unittest import TestCase, skipIf
 
 from appimagebuilder.utils.elf import get_arch
 
 
 class Test(TestCase):
-    def test_read_elf_arch(self):
+    @skipIf(not os.path.isfile("/lib/ld-linux.so.2"), "/lib/ld-linux.so.2 is required")
+    def test_read_elf_arch_i386(self):
         self.assertEqual("i386", get_arch("/lib/ld-linux.so.2"))
-        self.assertEqual("x86_64", get_arch("/lib64/ld-linux-x86-64.so.2"))
 
-        # self.assertEqual("i386", read_elf_arch("/tmp/AppRun-Release-i386"))
-        # self.assertEqual("x86_64", read_elf_arch("/tmp/AppRun-Release-x86_64"))
-        # self.assertEqual("aarch64", read_elf_arch("/tmp/AppRun-Release-aarch64"))
-        # self.assertEqual("gnueabihf", read_elf_arch("/tmp/AppRun-Release-gnueabihf"))
+    @skipIf(not os.path.isfile("/lib64/ld-linux-x86-64.so.2"), "/lib64/ld-linux-x86-64.so.2 is required")
+    def test_read_elf_arch_x86_64(self):
+        self.assertEqual("x86_64", get_arch("/lib64/ld-linux-x86-64.so.2"))
