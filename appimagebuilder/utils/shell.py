217587b834d1684f4e5334c6fcc49649330122e4
Alexis Lopez Zubieta <contact@azubieta.net>
2022-04-12 11:57:06 -0500

    ref: drop noop shel execute method and tests

--- appimagebuilder/utils/shell.py
+++ appimagebuilder/utils/shell.py
@@ -1,8 +1,5 @@
 import logging
-import os
 import shutil
-import subprocess
-import tempfile
 
 
 class CommandNotFoundError(RuntimeError):
@@ -36,34 +33,3 @@ def assert_successful_result(proc):
         raise RuntimeError(
             '"%s" execution failed with code %s' % (proc.args, proc.returncode)
         )
-
-
-def execute(script, env=None):
-    if env is None:
-        env = {}
-
-    if not script:
-        return
-
-    if isinstance(script, list):
-        script = "\n".join(script)
-
-    run_env = os.environ.copy()
-    for k, v in env.items():
-        run_env[k] = v
-
-    with tempfile.NamedTemporaryFile() as exported_env:
-        run_env["BUILDER_ENV"] = exported_env.name
-
-        _proc = subprocess.Popen(["bash", "-ve"], stdin=subprocess.PIPE, env=run_env)
-        _proc.communicate(script.encode())
-
-        if _proc.returncode != 0:
-            raise RuntimeError("Script exited with code: %s" % _proc.returncode)
-
-        exported_env.seek(0, 0)
-        for line in exported_env.readlines():
-            line = line.decode().strip()
-            logging.info("Exporting env: %s" % line)
-            key, val = line.split("=", 1)
-            os.environ[key] = val
