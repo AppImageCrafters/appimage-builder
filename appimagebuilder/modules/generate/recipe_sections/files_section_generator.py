f3d0378b8c94247bc70cfac7a2c690ba78e354dc
Alexis Lopez Zubieta <contact@azubieta.net>
2022-05-12 16:05:56 -0500

    Integrate File Dependencies Resolver with generator and deploy helper

--- appimagebuilder/modules/generate/recipe_sections/files_section_generator.py
+++ appimagebuilder/modules/generate/recipe_sections/files_section_generator.py
@@ -13,6 +13,7 @@ import fnmatch
 
 from appimagebuilder.modules.deploy import FileDeploy
 from appimagebuilder.context import BundleInfo
+from appimagebuilder.modules.deploy.files.dependencies_resolver.resolver import Resolver
 from appimagebuilder.modules.generate.recipe_sections.package_manager_recipe_section_generator import (
     PackageManagerSectionGenerator,
 )
@@ -38,12 +39,11 @@ class FilesSectionGenerator(PackageManagerSectionGenerator):
         return "files"
 
     def generate(self, dependencies: [str], bundle_info: BundleInfo) -> ({}, [str]):
-        include_list = [
-            str(path) for path in dependencies if not self._is_file_blacklisted(path)
-        ]
+        _root_files = self._exclude_resolvable_dependencies(dependencies)
+        _black_list_filter_result = self._exclude_blacklisted(_root_files)
 
         result = {
-            "include": sorted(include_list),
+            "include": sorted(_black_list_filter_result),
             "exclude": [
                 "usr/share/man",
                 "usr/share/doc/*/README.*",
@@ -54,8 +54,21 @@ class FilesSectionGenerator(PackageManagerSectionGenerator):
         }
         return result, []
 
+    def _exclude_blacklisted(self, dependencies):
+        return [
+            str(path) for path in dependencies if not self._is_file_blacklisted(path)
+        ]
+
     def _is_file_blacklisted(self, file_name):
         for pattern in self.exclusion_patterns:
             if fnmatch.fnmatch(file_name, pattern):
                 return True
         return False
+
+    def _exclude_resolvable_dependencies(self, _black_list_filter_result):
+        resolver = Resolver()
+        file_deps = resolver.resolve(_black_list_filter_result)
+        _root_files = set(_black_list_filter_result)
+        _root_files.difference_update(file_deps)
+
+        return _root_files
