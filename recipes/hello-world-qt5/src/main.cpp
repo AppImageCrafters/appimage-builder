d593ff6e3452dcf79fdb2765ad0a8902f95289dd
Alexis Lopez Zubieta <contact@azubieta.net>
2022-05-18 17:34:12 -0500

    add qt5 hello world recipe

---
+++ recipes/hello-world-qt5/src/main.cpp
@@ -0,0 +1,24 @@
+#include <QGuiApplication>
+#include <QQmlApplicationEngine>
+#include <QTimer>
+
+int main(int argc, char *argv[])
+{
+#if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
+    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+#endif
+
+    QGuiApplication app(argc, argv);
+
+    QQmlApplicationEngine engine;
+    const QUrl url(QStringLiteral("qrc:/main.qml"));
+    QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
+                     &app, [url](QObject *obj, const QUrl &objUrl) {
+        if (!obj && url == objUrl)
+            QCoreApplication::exit(-1);
+    }, Qt::QueuedConnection);
+    engine.load(url);
+
+    QTimer::singleShot(2000, &app, &QGuiApplication::quit);
+    return app.exec();
+}
