b623ef4ff773c2e4ee60d36c46affffa20b40a86
Alexis Lopez Zubieta <contact@azubieta.net>
2022-06-29 12:10:06 -0500

    feat: add glibstdc++ support

---
+++ appimagebuilder/modules/setup/apprun_3/helpers/glibstcpp_module.py
@@ -0,0 +1,85 @@
+#  Copyright  2022 Alexis Lopez Zubieta
+#
+#  Permission is hereby granted, free of charge, to any person obtaining a
+#  copy of this software and associated documentation files (the "Software"),
+#  to deal in the Software without restriction, including without limitation the
+#  rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+#  sell copies of the Software, and to permit persons to whom the Software is
+#  furnished to do so, subject to the following conditions:
+#
+#  The above copyright notice and this permission notice shall be included in
+#  all copies or substantial portions of the Software.
+import os
+import pathlib
+import shutil
+
+from appimagebuilder.context import Context
+from appimagebuilder.modules.setup import file_matching_patterns, apprun_utils
+from appimagebuilder.modules.setup.apprun_3.app_dir_info import AppDir
+from appimagebuilder.modules.setup.apprun_binaries_resolver import AppRunBinariesResolver
+from appimagebuilder.modules.setup.apprun_utils import replace_app_dir_in_path
+
+
+class AppRun3GLibStdCppSetupHelper:
+    def __init__(self, context: Context, app_dir_info: AppDir, apprun_modules_dir: pathlib.Path,
+                 binaries_resolver: AppRunBinariesResolver, arch: str):
+        self.context = context
+        self._apprun_modules_dir = apprun_modules_dir
+        self._module_dir = self._apprun_modules_dir / "glibstdcpp"
+        self._app_dir_info = app_dir_info
+        self._glibstdcpp_module_files = []
+        self._binaries_resolver = binaries_resolver
+        self._arch = arch
+
+    def setup(self):
+        self._glibstdcpp_module_files = self._app_dir_info.find(file_matching_patterns.glibstdcpp)
+
+        if self._glibstdcpp_module_files:
+            self._module_dir.mkdir(parents=True, exist_ok=True)
+
+            self._app_dir_info.move_files(self._glibstdcpp_module_files, self._module_dir)
+            self._deploy_check_glibstdcpp_binary()
+
+            libstdcpp_version = self._extract_libstdcpp_version()
+            library_paths = set([entry.path.parent for entry in self._glibstdcpp_module_files if entry.soname])
+
+            self._generate_glibstdcpp_module_config(libstdcpp_version, library_paths)
+
+    def _deploy_check_glibstdcpp_binary(self):
+        glibstdcpp_check_binary_path = self._binaries_resolver.resolve_check_glibstdcpp_binary(self._arch)
+        glibstdcpp_check_binary_target_path = self._module_dir / "check"
+
+        # ensure the target directory exists
+        glibstdcpp_check_binary_target_path.parent.mkdir(parents=True, exist_ok=True)
+
+        # copy  check glibc binary
+        shutil.copy(glibstdcpp_check_binary_path, glibstdcpp_check_binary_target_path)
+
+        # make binary executable
+        os.chmod(glibstdcpp_check_binary_target_path, 0o755)
+
+    def _generate_glibstdcpp_module_config(self, libstdcpp_version, library_paths):
+        library_paths = [replace_app_dir_in_path(self.context.app_dir, path) for path in library_paths]
+
+        config = {
+            "version": "1.0",
+            "check": {
+                "required_glibstdcpp": libstdcpp_version,
+            },
+            "module": {
+                "library_paths": library_paths,
+            },
+        }
+
+        # write the config file
+        glibstdcpp_module_config_path = self._module_dir / "config"
+        apprun_utils.write_config_file(config, glibstdcpp_module_config_path)
+
+    def _extract_libstdcpp_version(self):
+        version = None
+        for entry in self._glibstdcpp_module_files:
+            if entry.soname == 'libstdc++.so.6':
+                # extract libstdc++ version from file name
+                version = entry.path.name.split('.so.')[-1]
+                break
+        return version
