c9e2e399a2927fe8273963fc3a05e7c550e8df57
Alexis Lopez Zubieta <contact@azubieta.net>
2022-06-29 13:40:32 -0500

    Introduce AppRun3 Context

---
+++ appimagebuilder/modules/setup/apprun_3/apprun3_context.py
@@ -0,0 +1,45 @@
+#  Copyright  2022 Alexis Lopez Zubieta
+#
+#  Permission is hereby granted, free of charge, to any person obtaining a
+#  copy of this software and associated documentation files (the "Software"),
+#  to deal in the Software without restriction, including without limitation the
+#  rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+#  sell copies of the Software, and to permit persons to whom the Software is
+#  furnished to do so, subject to the following conditions:
+#
+#  The above copyright notice and this permission notice shall be included in
+#  all copies or substantial portions of the Software.
+import pathlib
+
+from appimagebuilder.context import Context
+from appimagebuilder.modules.setup.apprun_3.app_dir_info import AppDir
+from appimagebuilder.modules.setup.apprun_binaries_resolver import AppRunBinariesResolver
+
+
+class AppRun3Context:
+    build_context: Context = None
+    binaries_resolver: AppRunBinariesResolver = None
+    app_dir: AppDir = None
+
+    modules_dir: pathlib.Path = None
+    debug: bool = False
+
+    main_arch: str = None
+    architectures = set()
+
+    def __init__(self, build_context: Context):
+        self.build_context = build_context
+        self.modules_dir = build_context.app_dir / "opt"
+        self.debug = build_context.recipe.AppDir.runtime.debug() or False
+
+        # init AppRun binaries resolver
+        apprun_version = build_context.recipe.AppDir.runtime.version() or "v3.0.0"
+        build_context.build_dir / "AppRun" / apprun_version
+        self.binaries_resolver = AppRunBinariesResolver(apprun_version, self.debug, build_context.build_dir)
+
+        self.app_info = build_context.app_info
+
+        # information gathered during the setup process
+        self.bundle_archs = set(build_context.recipe.AppDir.runtime.architecture())
+
+        self.app_dir = AppDir(build_context.app_dir)
--- appimagebuilder/modules/setup/apprun_3/helpers/glibc_module.py
+++ appimagebuilder/modules/setup/apprun_3/helpers/glibc_module.py
@@ -18,36 +18,31 @@ import shutil
 import lief
 import packaging
 
-from appimagebuilder.context import Context
 from appimagebuilder.modules.setup import apprun_utils, file_matching_patterns
-from appimagebuilder.modules.setup.apprun_3.app_dir_info import AppDirFileInfo, AppDir
-from appimagebuilder.modules.setup.apprun_binaries_resolver import AppRunBinariesResolver
+from appimagebuilder.modules.setup.apprun_3.app_dir_info import AppDirFileInfo
+from appimagebuilder.modules.setup.apprun_3.apprun3_context import AppRun3Context
 from appimagebuilder.modules.setup.apprun_utils import replace_app_dir_in_path
 
 
 class AppRun3GLibCSetupHelper:
     """Setups the GLibC module for AppRun 3"""
 
-    def __init__(self, context: Context, app_dir_info: AppDir, apprun_modules_dir: pathlib.Path,
-                 binaries_resolver: AppRunBinariesResolver, arch: str):
+    def __init__(self, context: AppRun3Context):
         self.context = context
-        self._apprun_modules_dir = apprun_modules_dir
-        self._module_dir = self._apprun_modules_dir / "glibc"
-        self._app_dir_info = app_dir_info
+
+        self._module_dir = self.context.modules_dir / "glibc"
         self._glibc_module_files = []
-        self._binaries_resolver = binaries_resolver
-        self._arch = arch
 
     def setup(self):
         """Setups the glibc module"""
 
         # extract glibc module files
-        self._glibc_module_files = self._app_dir_info.find(file_matching_patterns.glibc)
+        self._glibc_module_files = self.context.app_dir.find(file_matching_patterns.glibc)
         if self._glibc_module_files:
             self._module_dir.mkdir(parents=True, exist_ok=True)
 
             self._patch_binaries_interpreter_path()
-            self._app_dir_info.move_files(self._glibc_module_files, self._module_dir)
+            self.context.app_dir.move_files(self._glibc_module_files, self._module_dir)
 
             # create links to the interpreters paths, so they can be found at runtime
             self._link_binary_interpreter_to_their_default_path()
@@ -61,7 +56,7 @@ class AppRun3GLibCSetupHelper:
     def _patch_binaries_interpreter_path(self):
         """Patches the binaries interpreter path on the AppDir"""
 
-        for file in self._app_dir_info.files.values():
+        for file in self.context.app_dir.files.values():
             if file.interpreter and not self._is_file_in_a_module(file) and not file.path.is_symlink():
                 binary = lief.parse(file.path.__str__())
                 self._patch_binary_interpreter_path(binary)
@@ -86,8 +81,8 @@ class AppRun3GLibCSetupHelper:
         return library_paths
 
     def _generate_glibc_module_config(self, library_paths):
-        library_paths = [replace_app_dir_in_path(self.context.app_dir, path) for path in library_paths]
-        runtime_dir = "$APPDIR/" + self._module_dir.relative_to(self.context.app_dir).__str__()
+        library_paths = [replace_app_dir_in_path(self.context.app_dir.path, path) for path in library_paths]
+        runtime_dir = "$APPDIR/" + self._module_dir.relative_to(self.context.app_dir.path).__str__()
         config = {
             "version": "1.0",
             "check": {
@@ -149,8 +144,8 @@ class AppRun3GLibCSetupHelper:
         """Links the binary interpreter to their default path"""
 
         sys_root = pathlib.Path('/')
-        for binary_interpreter in self._app_dir_info.binary_interpreters:
-            binary_interpreter_path = self.context.app_dir / binary_interpreter.__str__().strip("/")
+        for binary_interpreter in self.context.app_dir.binary_interpreters:
+            binary_interpreter_path = self.context.app_dir.path / binary_interpreter.__str__().strip("/")
 
             # ensure the binary interpreter dir exists
             binary_interpreter_path.parent.mkdir(parents=True, exist_ok=True)
@@ -160,11 +155,11 @@ class AppRun3GLibCSetupHelper:
     def _link_script_interpreters_to_their_path(self):
         """Links the script interpreters to their path"""
 
-        for interpreter_path in self._app_dir_info.script_interpreters:
+        for interpreter_path in self.context.app_dir.script_interpreters:
             rel_path = pathlib.Path(interpreter_path.strip("/"))
-            expected_path = self.context.app_dir / rel_path
+            expected_path = self.context.app_dir.path / rel_path
             if expected_path.exists():
-                mirror_path = self._apprun_modules_dir / "glibc" / rel_path
+                mirror_path = self._module_dir / rel_path
                 rel_mirror_path = os.path.relpath(expected_path, mirror_path.parent)
 
                 mirror_path.parent.mkdir(parents=True, exist_ok=True)
@@ -176,7 +171,7 @@ class AppRun3GLibCSetupHelper:
     def _deploy_check_glibc_binary(self):
         """Deploys the glibc check binary"""
 
-        glibc_check_binary_path = self._binaries_resolver.resolve_check_glibc_binary(self._arch)
+        glibc_check_binary_path = self.context.binaries_resolver.resolve_check_glibc_binary(self.context.main_arch)
         glibc_check_binary_target_path = self._module_dir / "check"
 
         # ensure the target directory exists
@@ -192,4 +187,4 @@ class AppRun3GLibCSetupHelper:
         """Checks if a file belongs to a module"""
 
         path_str = file.path.__str__()
-        return path_str.startswith(self._apprun_modules_dir.__str__())
+        return path_str.startswith(self.context.modules_dir.__str__())
