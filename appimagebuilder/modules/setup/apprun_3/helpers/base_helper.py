816d0533176d5a3df849d1e5304e4778e47e3eb7
Alexis Lopez Zubieta <contact@azubieta.net>
2022-06-29 18:13:19 -0500

    fix: add missing base AppRun 3 helper

---
+++ appimagebuilder/modules/setup/apprun_3/helpers/base_helper.py
@@ -0,0 +1,20 @@
+#  Copyright  2020 Alexis Lopez Zubieta
+#
+#  Permission is hereby granted, free of charge, to any person obtaining a
+#  copy of this software and associated documentation files (the "Software"),
+#  to deal in the Software without restriction, including without limitation the
+#  rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+#  sell copies of the Software, and to permit persons to whom the Software is
+#  furnished to do so, subject to the following conditions:
+#
+#  The above copyright notice and this permission notice shall be included in
+#  all copies or substantial portions of the Software.
+from appimagebuilder.modules.setup.apprun_3.apprun3_context import AppRun3Context
+
+
+class AppRun3Helper:
+    def __init__(self, context: AppRun3Context):
+        self.context = context
+
+    def run(self):
+        pass
