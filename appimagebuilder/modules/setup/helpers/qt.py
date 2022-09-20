987badfd9f064bd602cccf611bb6f2aef2e6f576
caminac <caminac@gmail.com>
2022-09-20 16:28:28 +0200

    extended to qt4

--- appimagebuilder/modules/setup/helpers/qt.py
+++ appimagebuilder/modules/setup/helpers/qt.py
@@ -23,12 +23,19 @@ class Qt(BaseHelper):
     def __init__(self, app_dir, app_dir_files):
         super().__init__(app_dir, app_dir_files)
         self.app_dir = Path(app_dir)
+        self._qt4_dirs = {}
         self._qt5_dirs = {}
         self._qt6_dirs = {}
 
     def configure(self, env: Environment, preserve_files):
+        self._configure_qt4()
         self._configure_qt5()
         self._configure_qt6()
+
+    def _configure_qt4(self):
+        self._locate_qt4_dirs()
+        if self._qt4_dirs:
+            pass
 
     def _configure_qt5(self):
         self._locate_qt5_dirs()
@@ -67,6 +74,53 @@ class Qt(BaseHelper):
             config[k] = v.relative_to(self.app_dir)
 
         return config
+
+    def _locate_qt4_dirs(self):
+        libqt4core_path = self.finder.find_one(
+            "*/libQtCore.so.4.*", [Finder.is_file, Finder.is_elf_shared_lib]
+        )
+        if libqt4core_path:
+            self._qt4_dirs["Libraries"] = libqt4core_path.parent
+        else:
+            # don't go any forward if libQtCore.so.4.... is not found
+            return
+
+        # ??
+        # qtwebengine_path = self.finder.find_one(
+        #     "*/QtWebEngineProcess", [Finder.is_file, Finder.is_executable]
+        # )
+        # if qtwebengine_path:
+        #     self._qt4_dirs["LibraryExecutables"] = qtwebengine_path.parent
+
+        qmake_path = self.finder.find_one(
+            "*/qmake", [Finder.is_file, Finder.is_executable]
+        )
+        if qmake_path:
+            self._qt4_dirs["Binaries"] = qmake_path.parent
+
+        # ??
+        libqminimal_path = self.finder.find_one(
+            "*/libqminimal.so", [Finder.is_file, Finder.is_elf]
+        )
+        if libqminimal_path:
+            self._qt4_dirs["Plugins"] = libqminimal_path.parent.parent
+
+        builtins_qmltypes_path = self.finder.find_one(
+            "*/builtins.qmltypes", [Finder.is_file]
+        )
+        if builtins_qmltypes_path:
+            self._qt4_dirs["Qml2Imports"] = builtins_qmltypes_path.parent
+
+        # ok
+        qtbase_translations_path = self.finder.find_one(
+            "*/qt4/translations", [Finder.is_dir]
+        )
+        if qtbase_translations_path:
+            self._qt4_dirs["Translations"] = qtbase_translations_path
+
+        data_path = self.finder.find_one("*/qt4/resources", [Finder.is_dir])
+        if data_path:
+            self._qt4_dirs["Data"] = data_path.parent
 
     def _locate_qt5_dirs(self):
         libqt5core_path = self.finder.find_one(
