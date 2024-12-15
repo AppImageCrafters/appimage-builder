648fdaf544851f539c792146136c92ec1c697a2d
DrSheppard <60893791+Drsheppard01@users.noreply.github.com>
2024-12-16 02:45:55 +0300

    Update appimage.py
    Resolve #364

--- appimagebuilder/modules/appimage.py
+++ appimagebuilder/modules/appimage.py
@@ -88,7 +88,7 @@ class AppImageCreator:
         return str(runtime_path)
 
     def _get_runtime_url(self):
-        runtime_url_template = "https://github.com/AppImage/AppImageKit/releases/download/continuous/runtime-%s"
+        runtime_url_template = "https://github.com/AppImage/type2-runtime/releases/tag/continuous/runtime-%s"
         runtime_url = runtime_url_template % self.target_arch
         return runtime_url
 
