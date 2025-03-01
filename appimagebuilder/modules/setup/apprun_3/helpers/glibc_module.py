a31c406554c07c3671f13e455045e7fc86bab836
Dmitrii Sharshakov <d3dx12.xx@gmail.com>
2025-03-01 19:08:49 +0100

    chore: bump deps
    Also fix LIEF API usage

--- appimagebuilder/modules/setup/apprun_3/helpers/glibc_module.py
+++ appimagebuilder/modules/setup/apprun_3/helpers/glibc_module.py
@@ -60,16 +60,16 @@ class AppRun3GLibCSetupHelper(AppRun3Helper):
         for file in self.context.app_dir.files.values():
             if file.interpreter and not self._is_file_in_a_module(file) and not file.path.is_symlink():
                 binary = lief.parse(file.path.__str__())
-                self._patch_binary_interpreter_path(binary)
+                self._patch_binary_interpreter_path(binary, file.path.__str__())
 
-    def _patch_binary_interpreter_path(self, binary):
+    def _patch_binary_interpreter_path(self, binary, output):
         """Patch the interpreter of a binary making it relative"""
 
         interpreter = binary.interpreter
         new_interpreter = interpreter.lstrip("/")
 
         binary.interpreter = new_interpreter
-        binary.write(binary.name)
+        binary.write(output)
 
     def _extract_library_paths_from_glibc_module_files(self):
         """Extracts library paths from glibc module files"""
